092397489fbb956a6357c2a32340659e8cc7ae91
Roman Martsinovich <martsinovichroman@gmail.com>
2020-03-11 19:53:53 +0300

    Structure request

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -7,13 +7,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DynamicFormComponent implements OnInit {
 
-  structureName: string;
+
 
   constructor() {
   }
 
   ngOnInit(): void {
-    console.log(this.structureName);
+
   }
 
 }
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {RestService} from '../service/rest.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
-export class PopupComponent implements OnInit {
+export class PopupComponent implements OnInit, OnDestroy {
 
-  constructor(public activeModal: NgbActiveModal) { }
+  structureName: string;
+  sub: Subscription;
+
+  constructor(public activeModal: NgbActiveModal, private restService: RestService) {
+  }
 
   ngOnInit(): void {
+    const sub = this.restService.getStructure(this.structureName).subscribe(structure => {
+
+    });
+    console.log(this.structureName);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
